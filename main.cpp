3978308493a630a764c8b51502adbc823ac30cca
elisa <e.arca3@studenti.unica.it>
2023-05-26 10:19:48 +0200

    aggiunta tre bottoni unione, intersezione, sottrazione

--- main.cpp
+++ main.cpp
@@ -12,19 +12,16 @@
 
 int main(int argc, char **argv)
 {
+    //mesh iniziali
     std::vector<std::string> files;
-
     files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/bunny.obj");
     files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/cow.obj");
-
 
     using namespace cinolib;
 
     DrawableTrimesh<> m(files[0].c_str());
     DrawableTrimesh<> m1(files[1].c_str());
     DrawableTrimesh<> m2;
-    //DrawableTrimesh<> m2("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/cube.obj");
-
 
     GLcanvas gui;
     SurfaceMeshControls<DrawableTrimesh<>> mesh_controls(&m, &gui);
@@ -37,134 +34,77 @@ int main(int argc, char **argv)
     m.poly_set_color(cinolib::Color::PASTEL_PINK());
     m1.poly_set_color(cinolib::Color::PASTEL_VIOLET());
 
-    BoolOp op;
-    std::string file_out;
+    std::vector<double> in_coords, bool_coords;
+    std::vector<uint> in_tris, bool_tris;
+    std::vector<uint> in_labels;
+    std::vector<std::bitset<NBIT>> bool_labels;
 
-    //operazioni booleane possibili
-    //op = INTERSECTION;
-    //op = UNION;
-    op = SUBTRACTION;
-    //op = XOR;
+    //"riempie i vettori" usando le mesh presenti in files. Dovrà essere caricato su una mesh
+    loadMultipleFiles(files, in_coords, in_tris, in_labels);
+
+    initFPU();
+
+    point_arena arena;
+    std::vector<genericPoint*> arr_verts; // <- it contains the original expl verts + the new_impl verts
+    std::vector<uint> arr_in_tris, arr_out_tris;
+    std::vector<std::bitset<NBIT>> arr_in_labels;
+    std::vector<DuplTriInfo> dupl_triangles;
+    Labels labels;
+    std::vector<phmap::flat_hash_set<uint>> patches;
+    cinolib::FOctree octree; // built with arr_in_tris and arr_in_labels
+
+    customArrangementPipeline(in_coords, in_tris, in_labels, arr_in_tris, arr_in_labels, arena, arr_verts,
+                              arr_out_tris, labels, octree, dupl_triangles);
+
+    FastTrimesh tm(arr_verts, arr_out_tris, true);
+
+    computeAllPatches(tm, labels, patches, true);
+
+    // the informations about duplicated triangles (removed in arrangements) are restored in the original structures
+    addDuplicateTrisInfoInStructures(dupl_triangles, arr_in_tris, arr_in_labels, octree);
+
+    // parse patches with octree and rays
+    cinolib::vec3d max_coords(octree.nodes[0].bbox.max.x() +0.5, octree.nodes[0].bbox.max.y() +0.5, octree.nodes[0].bbox.max.z() +0.5);
+    computeInsideOut(tm, patches, octree, arr_verts, arr_in_tris, arr_in_labels, max_coords, labels);
+
+    uint num_tris_in_final_solution;
 
     gui.callback_app_controls = [&]()
     {
-        if(ImGui::Button("Operazione booleana"))
-        {
 
-            //button clicked:
-            //fai l'operazione booleana tra le mesh
-            //metti il risultato in m2
-
-            //dichiaro questi
-            std::vector<double> in_coords, bool_coords;
-            std::vector<uint> in_tris, bool_tris;
-            std::vector<uint> in_labels;
-            std::vector<std::bitset<NBIT>> bool_labels;
-
-            //"riempie i vettori" usando le mesh presenti in files. Dovrà essere caricato su una mesh
-            loadMultipleFiles(files, in_coords, in_tris, in_labels);
-
-            initFPU();
-
-            point_arena arena;
-            std::vector<genericPoint*> arr_verts; // <- it contains the original expl verts + the new_impl verts
-            std::vector<uint> arr_in_tris, arr_out_tris;
-            std::vector<std::bitset<NBIT>> arr_in_labels;
-            std::vector<DuplTriInfo> dupl_triangles;
-            Labels labels;
-            std::vector<phmap::flat_hash_set<uint>> patches;
-            cinolib::FOctree octree; // built with arr_in_tris and arr_in_labels
-
-            customArrangementPipeline(in_coords, in_tris, in_labels, arr_in_tris, arr_in_labels, arena, arr_verts,
-                                      arr_out_tris, labels, octree, dupl_triangles);
-
-            FastTrimesh tm(arr_verts, arr_out_tris, true);
-
-            computeAllPatches(tm, labels, patches, true);
-
-            // the informations about duplicated triangles (removed in arrangements) are restored in the original structures
-            addDuplicateTrisInfoInStructures(dupl_triangles, arr_in_tris, arr_in_labels, octree);
-
-            // parse patches with octree and rays
-            cinolib::vec3d max_coords(octree.nodes[0].bbox.max.x() +0.5, octree.nodes[0].bbox.max.y() +0.5, octree.nodes[0].bbox.max.z() +0.5);
-            computeInsideOut(tm, patches, octree, arr_verts, arr_in_tris, arr_in_labels, max_coords, labels);
-            // booleand operations
-            uint num_tris_in_final_solution;
-            if(op == INTERSECTION)
-                num_tris_in_final_solution = boolIntersection(tm, labels);
-            else if(op == UNION)
-                num_tris_in_final_solution = boolUnion(tm, labels);
-            else if(op == SUBTRACTION)
-                num_tris_in_final_solution = boolSubtraction(tm, labels);
-            else if(op == XOR)
-                num_tris_in_final_solution = boolXOR(tm, labels);
-            else
-            {
-                std::cerr << "boolean operation not implemented yet" << std::endl;
-                std::exit(EXIT_FAILURE);
-            }
-
+        if(ImGui::Button("INTERSECTION")) {
+            num_tris_in_final_solution = boolIntersection(tm, labels);
             computeFinalExplicitResult(tm, labels, num_tris_in_final_solution, bool_coords, bool_tris, bool_labels, true);
+            m2 = DrawableTrimesh(bool_coords, bool_tris);
+            m2.poly_set_color(cinolib::Color::PASTEL_RED());
 
-            //file_out = "/Users/elisa/Desktop/Tirocinio/booleans_demo/data/result.obj";
-
-            //scrive il risultato in un file
-           // cinolib::write_OBJ(file_out.c_str(), bool_coords, bool_tris, {});
-
-           m2 = DrawableTrimesh(bool_coords, bool_tris);
-
-
-            //DrawableTrimesh<> m2("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/result.obj");
-            m2.poly_set_color(cinolib::Color::PASTEL_PINK());
-
-            //devo mettere file_out su una mesh
-            //string -> obj
-
-
-            //si tolgono m e m1 dalla gui e si inserisce m2
             gui.pop(&m);
             gui.pop(&m1);
+            gui.push(&m2);
+        }
 
+        if(ImGui::Button("UNION")) {
+            num_tris_in_final_solution = boolUnion(tm, labels);
+            computeFinalExplicitResult(tm, labels, num_tris_in_final_solution, bool_coords, bool_tris, bool_labels, true);
+            m2 = DrawableTrimesh(bool_coords, bool_tris);
+            m2.poly_set_color(cinolib::Color::PASTEL_GREEN());
 
+            gui.pop(&m);
+            gui.pop(&m1);
+            gui.push(&m2);
+        }
+
+        if(ImGui::Button("SUBTRACTION")) {
+            num_tris_in_final_solution = boolSubtraction(tm, labels);
+            computeFinalExplicitResult(tm, labels, num_tris_in_final_solution, bool_coords, bool_tris, bool_labels, true);
+            m2 = DrawableTrimesh(bool_coords, bool_tris);
+            m2.poly_set_color(cinolib::Color::PASTEL_YELLOW());
+
+            gui.pop(&m);
+            gui.pop(&m1);
             gui.push(&m2);
         }
     };
 
     return gui.launch();
 }
-    /*
-
-
-    if(argc < 5)
-    {
-        std::cout << "syntax error!" << std::endl;
-        std::cout << "./exact_boolean BOOL_OPERATION (intersection OR union OR subtraction) input1.obj input2.obj output.obj" << std::endl;
-        return -1;
-    }
-    else
-    {
-        if (strcmp(argv[1], "intersection") == 0)       op = INTERSECTION;
-        else if (strcmp(argv[1], "union") == 0)         op = UNION;
-        else if (strcmp(argv[1], "subtraction") == 0)   op = SUBTRACTION;
-        else if (strcmp(argv[1], "xor") == 0)           op = XOR;
-    }*/
-
-    /*for(int i = 2; i < (argc -1); i++)
-        files.emplace_back(argv[i]);
-
-    file_out = argv[argc-1];*/
-
-    /*
-    std::vector<double> in_coords, bool_coords;
-    std::vector<uint> in_tris, bool_tris;
-    std::vector<uint> in_labels;
-    std::vector<std::bitset<NBIT>> bool_labels;
-
-    loadMultipleFiles(files, in_coords, in_tris, in_labels);
-
-    booleanPipeline(in_coords, in_tris, in_labels, op, bool_coords, bool_tris, bool_labels);
-
-    cinolib::write_OBJ(file_out.c_str(), bool_coords, bool_tris, {});*/
-
-   // return 0;
-//}
