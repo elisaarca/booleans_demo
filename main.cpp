bcd1b691625d51ca47699f102d7363dbc3dfdf29
elisa <e.arca3@studenti.unica.it>
2023-07-12 14:36:22 +0200

    funziona scalatura e selezione punto forse

--- main.cpp
+++ main.cpp
@@ -19,19 +19,27 @@ int main(int argc, char **argv)
     //mesh iniziali
     std::vector<std::string> files;
     files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/bunny.obj");
-    //files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/cow.obj");
+    files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/cow.obj");
     files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/sfera.obj");
 
     using namespace cinolib;
 
+    //coniglio
     DrawableTrimesh<> m(files[0].c_str());
-    DrawableTrimesh<> m1(files[1].c_str());
-    DrawableTrimesh<> m2;
+    //mucca
+    DrawableTrimesh<> m2(files[1].c_str());
+    //sfera
+    DrawableTrimesh<> m3(files[2].c_str());
+
+
+    DrawableTrimesh<> m1;
+
+    DrawableTrimesh<> mresult;
 
     GLcanvas gui;
     SurfaceMeshControls<DrawableTrimesh<>> mesh_controls(&m, &gui);
     float current_size = 0.1;
-    m1.scale(0.1);
+
 
     //le due mesh vengono pushate nella gui
     gui.push(&m);
@@ -44,6 +52,7 @@ int main(int argc, char **argv)
     m.poly_set_color(cinolib::Color::PASTEL_PINK());
     //m1.poly_set_color(cinolib::Color::PASTEL_VIOLET());
 
+
     //tutto questo mi serve per salvare le informazioni delle mesh su cui sto lavorando
     std::vector<double> in_coords, bool_coords;
     std::vector<uint> in_tris, bool_tris;
@@ -52,7 +61,7 @@ int main(int argc, char **argv)
 
     //"riempie i vettori" usando le mesh presenti in files. Dovrà essere caricato su una mesh
     loadMultipleFiles(files, in_coords, in_tris, in_labels);
-
+    m.updateGL();
     initFPU();
 
     point_arena arena;
@@ -64,75 +73,55 @@ int main(int argc, char **argv)
     std::vector<phmap::flat_hash_set<uint>> patches;
     cinolib::FOctree octree; // built with arr_in_tris and arr_in_labels
 
-    customArrangementPipeline(in_coords, in_tris, in_labels, arr_in_tris, arr_in_labels, arena, arr_verts,
-                              arr_out_tris, labels, octree, dupl_triangles);
 
-    FastTrimesh tm(arr_verts, arr_out_tris, true);
+      customArrangementPipeline(in_coords, in_tris, in_labels, arr_in_tris, arr_in_labels, arena, arr_verts,
+                                arr_out_tris, labels, octree, dupl_triangles);
 
-    computeAllPatches(tm, labels, patches, true);
+      FastTrimesh tm(arr_verts, arr_out_tris, true);
 
-    // the informations about duplicated triangles (removed in arrangements) are restored in the original structures
-    addDuplicateTrisInfoInStructures(dupl_triangles, arr_in_tris, arr_in_labels, octree);
+      computeAllPatches(tm, labels, patches, true);
 
-    // parse patches with octree and rays
-    cinolib::vec3d max_coords(octree.nodes[0].bbox.max.x() +0.5, octree.nodes[0].bbox.max.y() +0.5, octree.nodes[0].bbox.max.z() +0.5);
-    computeInsideOut(tm, patches, octree, arr_verts, arr_in_tris, arr_in_labels, max_coords, labels);
+      // the informations about duplicated triangles (removed in arrangements) are restored in the original structures
+      addDuplicateTrisInfoInStructures(dupl_triangles, arr_in_tris, arr_in_labels, octree);
 
-    //operazioni(files, in_coords, in_tris, in_labels, &labels);
-    uint num_tris_in_final_solution;
+      // parse patches with octree and rays
+      cinolib::vec3d max_coords(octree.nodes[0].bbox.max.x() +0.5, octree.nodes[0].bbox.max.y() +0.5, octree.nodes[0].bbox.max.z() +0.5);
+      computeInsideOut(tm, patches, octree, arr_verts, arr_in_tris, arr_in_labels, max_coords, labels);
+
+      //operazioni(files, in_coords, in_tris, in_labels, &labels);
+      uint num_tris_in_final_solution;
+
 
     gui.callback_app_controls = [&]()
     {
 
+        if(ImGui::Button("mucca")) {
+            m1 = m2;
+            gui.pop(&m1);
+            m1.scale(0.1);
+            m1.updateGL();
+        }
+
+        if(ImGui::Button("sfera")) {
+            m1 = m3;
+            gui.pop(&m1);
+            m1.scale(0.1);
+            m1.updateGL();
+        }
+
         if(ImGui::Button("INTERSECTION")) {
 
-            std::vector<double> in_coords, bool_coords;
-            std::vector<uint> in_tris, bool_tris;
-            std::vector<uint> in_labels;
-            std::vector<std::bitset<NBIT>> bool_labels;
-
-
-            //"riempie i vettori" usando le mesh presenti in files. Dovrà essere caricato su una mesh
-            loadMultipleFiles(files, in_coords, in_tris, in_labels);
-
-            initFPU();
-
-            point_arena arena;
-            std::vector<genericPoint*> arr_verts; // <- it contains the original expl verts + the new_impl verts
-            std::vector<uint> arr_in_tris, arr_out_tris;
-            std::vector<std::bitset<NBIT>> arr_in_labels;
-            std::vector<DuplTriInfo> dupl_triangles;
-            Labels labels;
-            std::vector<phmap::flat_hash_set<uint>> patches;
-            cinolib::FOctree octree; // built with arr_in_tris and arr_in_labels
-
-            customArrangementPipeline(in_coords, in_tris, in_labels, arr_in_tris, arr_in_labels, arena, arr_verts,
-                                      arr_out_tris, labels, octree, dupl_triangles);
-
-            FastTrimesh tm(arr_verts, arr_out_tris, true);
-
-            computeAllPatches(tm, labels, patches, true);
-
-            // the informations about duplicated triangles (removed in arrangements) are restored in the original structures
-            addDuplicateTrisInfoInStructures(dupl_triangles, arr_in_tris, arr_in_labels, octree);
-
-            // parse patches with octree and rays
-            cinolib::vec3d max_coords(octree.nodes[0].bbox.max.x() +0.5, octree.nodes[0].bbox.max.y() +0.5, octree.nodes[0].bbox.max.z() +0.5);
-            computeInsideOut(tm, patches, octree, arr_verts, arr_in_tris, arr_in_labels, max_coords, labels);
-
-
-            //////////
             num_tris_in_final_solution = boolIntersection(tm, labels);
             computeFinalExplicitResult(tm, labels, num_tris_in_final_solution, bool_coords, bool_tris, bool_labels, true);
-            m2 = DrawableTrimesh(bool_coords, bool_tris);
-            m2.poly_set_color(cinolib::Color::PASTEL_RED());
+            mresult = DrawableTrimesh(bool_coords, bool_tris);
+            mresult.poly_set_color(cinolib::Color::PASTEL_GREEN());
 
             gui.pop(&m);
             gui.pop(&m1);
-            gui.push(&m2);
+            gui.push(&mresult);
         }
-/*
-        if(ImGui::Button("UNION")) {
+
+        /*if(ImGui::Button("UNION")) {
             num_tris_in_final_solution = boolUnion(tm, labels);
             computeFinalExplicitResult(tm, labels, num_tris_in_final_solution, bool_coords, bool_tris, bool_labels, true);
             m2 = DrawableTrimesh(bool_coords, bool_tris);
@@ -144,22 +133,22 @@ int main(int argc, char **argv)
             gui.pop(&m);
             gui.pop(&m1);
             gui.push(&m2);
-        }
+        }*/
 
-        if(ImGui::Button("SUBTRACTION")) {
+        /*if(ImGui::Button("SUBTRACTION")) {
             num_tris_in_final_solution = boolSubtraction(tm, labels);
             computeFinalExplicitResult(tm, labels, num_tris_in_final_solution, bool_coords, bool_tris, bool_labels, true);
-            m2 = DrawableTrimesh(bool_coords, bool_tris);
-            m2.poly_set_color(cinolib::Color::PASTEL_YELLOW());
+            mresult = DrawableTrimesh(bool_coords, bool_tris);
+            mresult.poly_set_color(cinolib::Color::PASTEL_YELLOW());
 
             gui.pop(&m);
             gui.pop(&m1);
-            gui.push(&m2);
+            gui.push(&mresult);
         }*/
 
         ///Scale
         ImGui::Text("Stencil size");
-        if(ImGui::SliderFloat("##size", &current_size, 0.1, 1)){
+        if(ImGui::SliderFloat("##size", &current_size, 0.01, 1)){
 
             m1.scale( current_size / m1.bbox().diag());
             m1.updateGL();
@@ -168,21 +157,20 @@ int main(int argc, char **argv)
         ///Reset
         if(ImGui::SmallButton("Reset"))
         {
-            m.vert_set_color(Color::WHITE());
             gui.pop(&m);
             gui.pop(&m1);
 
             files.clear();
+
             files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/bunny.obj");
             files.push_back("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/sfera.obj");
+            m = DrawableTrimesh("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/bunny.obj");
+            m2= DrawableTrimesh("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/cow.obj");
+            m3 = DrawableTrimesh("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/sfera.obj");
 
-            m1 = DrawableTrimesh("/Users/elisa/Desktop/Tirocinio/booleans_demo/data/sfera.obj");
             gui.push(&m);
-            gui.push(&m1);
             m.updateGL();
-            m1.updateGL();
         }
-
     };
 
     //trovo la posizione del centro della sfera e la sottraggo alle coordinate del punto in cui clicco.
@@ -192,6 +180,7 @@ int main(int argc, char **argv)
     Profiler profiler;
     gui.callback_mouse_left_click = [&](int modifiers) -> bool
     {
+        gui.pop(&m1);
         if(modifiers & GLFW_MOD_SHIFT)
         {
             vec3d p;
@@ -202,7 +191,7 @@ int main(int argc, char **argv)
                 //profiler.push("Vertex pick"); -> probabilmente serve per verificare le prestazioni
                 uint vid = m.pick_vert(p);
                 //profiler.pop(); -> probabilmente serve per verificare le prestazioni
-                std::cout << "ID " << vid << std::endl; //stampa l'id del veritice ottenuto nella console
+                std::cout << "ID " << vid << std::endl; //stampa l'id del vertice ottenuto nella console
 
                 //coordinate del centro della sfera
                 vec3d center = m1.bbox().center();
@@ -213,10 +202,12 @@ int main(int argc, char **argv)
                 m1.translate(delta);
 
                //m.vert_data(vid).color = Color::RED();
+
                gui.push(&m1);
 
-               m.updateGL();
+               //m.updateGL();
                m1.updateGL();
+
             }
         }
         return false;
